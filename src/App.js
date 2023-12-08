04699fc18643d7978634ddc9a6b62e28cdf29430
Jordan <jordan@smartdust.me>
2023-12-08 21:00:47 +0100

    Added images and edited text

--- src/App.js
+++ src/App.js
@@ -3,23 +3,28 @@ const images = require.context('./static', true);
 const imageList = images.keys().map(image => images(image));
 
 function App() {
+    console.log(imageList)
     return (
         <div className="App">
-            <h1 className="display-3 text-center my-4">Mieszkanie do wykończenia na Bysławskiej 12</h1>
+            <h1 className="display-3 text-center my-4">Mieszkanie na Bysławskiej 12</h1>
+            <h3 className="text-center mb-4">Przestronne, dwupoziomowe mieszkanie ok. 130 m<sup>2</sup>, w stanie deweloperskim, zlokalizowane w nowej, kameralnej inwestycji.<br></br></h3>
             <p className="mx-5">
-                Przestronne, dwupoziomowe mieszkanie zlokalizowane w kameralnej inwestycji wśród zieleni.<br></br>
-                Wygodny i nieodległy dojazd do Miasteczka przez trasę mostu Południowego.<br></br>
-                Dodatkowo, dogodna bliskość przystangów komunikacji miejskiej.<br></br>
+                Wygodny i nieodległy dojazd do Miasteczka przez trasę mostu Południowego.
+                Dogodna bliskość przystanków komunikacji miejskiej i podstawowej infrastruktury.
+            </p>
+            <p className="mx-5">
                 Osiedle ogrodzone i monitorowane, z troską zarządzane przez Wspólnotę Mieszkaniową.
-                Do wyłącznego korzystania przez przyszłego właściciela należy przedogródek ok. 36 m<sup>2</sup> oraz 2 wydzielone miejsca postojowe na parkingu zewnętrznym (w cenie).<br></br>
-                Wystawa północno-południowa.
             </p>
             <p className="mx-5">
                 Układ mieszkania obejmuje 3 poziomy:<br/>
                 0 - wewnętrzne schody wejściowe na I p., wnęka na garderobę.<br/>
                 I - pokój z kominkiem i wyjściem na balkon; osobny gabinet, kuchnia z wyjściem na loggię, jadalnia, garderoba, schowek, WC,<br/>
-                II - 3 sypialnie, w tym jedna z wyjściem na balkon, dwie pełne łazienki (w tym jedna z oknem), pełna wysokość skosu w najmniejszym pokoju.<br/>
-                Duże, panoramiczne, trzyszybowe przeszklania.
+                II - 3 sypialnie, w tym jedna z wyjściem na balkon, dwie pełne łazienki (jedna z oknem)<br/>
+                Duże, panoramiczne, trzyszybowe przeszklenia. Urocze widoki na zabudowę jednorodzinną i tereny zielone.
+                Wystawa okien północno-południowa.
+            </p>
+            <p className="mx-5">
+            Do wyłącznego korzystania przez przyszłego właściciela należy przedogródek ok. 36 m<sup>2</sup> oraz 2 wydzielone miejsca postojowe na parkingu zewnętrznym (w cenie).
             </p>
             <p className="mx-5 text-center">
                 Pełna własność, KW bez obciążeń.<br/>
