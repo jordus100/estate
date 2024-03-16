d9be9e52c53449c75e3ab4d8089cdc931bf2771e
Jordan <jordan@smartdust.me>
2024-03-16 17:07:18 +0100

    Corrections to content, remove snow and change bg color

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
-import Snowfall from "react-snowfall";
 const images = require.context('./static', true);
 const imageList = images.keys().map(image => images(image));
 
 function App() {
     return (
         <div className="App">
-            <Snowfall />
             <h1 className="display-3 text-center my-4">Mieszkanie na Bysławskiej 12</h1>
             <h3 className="text-center mb-4">Przestronne, dwupoziomowe mieszkanie ok. 130 m<sup>2</sup>, w stanie deweloperskim, zlokalizowane w nowej, kameralnej inwestycji.<br></br></h3>
             <p className="mx-5">
@@ -17,12 +15,14 @@ function App() {
                 Osiedle ogrodzone i monitorowane, z troską zarządzane przez Wspólnotę Mieszkaniową.
             </p>
             <p className="mx-5">
-                Układ mieszkania obejmuje 3 poziomy:<br/>
-                0 - wewnętrzne schody wejściowe na I p., wnęka na garderobę.<br/>
+                Układ mieszkania obejmuje 2 poziomy:<br/>
                 I - pokój z kominkiem i wyjściem na balkon; osobny gabinet, kuchnia z wyjściem na loggię, jadalnia, garderoba, schowek, WC,<br/>
                 II - 3 sypialnie, w tym jedna z wyjściem na balkon, dwie pełne łazienki (jedna z oknem)<br/>
+                Wejście do mieszkania jest z indywidualnej klatki schodowej.<br/>
+            </p>
+            <p className="mx-5">
                 Duże, panoramiczne, trzyszybowe przeszklenia. Urocze widoki na zabudowę jednorodzinną i tereny zielone.
-                Wystawa okien północno-południowa.
+                Wystawa okien na południe i północ.
             </p>
             <p className="mx-5">
             Do wyłącznego korzystania przez przyszłego właściciela należy przedogródek ok. 36 m<sup>2</sup> oraz 2 wydzielone miejsca postojowe na parkingu zewnętrznym (w cenie).
